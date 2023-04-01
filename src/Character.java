44da4e34ea4a7118cfdd3c24a938fcc1ea550678
IbraTinou <gatboismartin@gmail.com>
2023-04-01 18:24:44 +0200

    Implement abstract attack methods

--- src/Character.java
+++ src/Character.java
@@ -2,7 +2,8 @@ public abstract class Character {
     public String name;
     public int maxHp,hp;
 
-    public abstract int attack();
+    public abstract int attack(Character character);
+    public abstract int attack(Character character, Spell spell);
 
     public Character (String name,int maxHp){
         this.name = name;
